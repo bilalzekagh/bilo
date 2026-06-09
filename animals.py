b5df2aec7587b94fa7197dec344066cbd9ef5569
bilalzekagh <bilalzekagh@gmail.com>
2026-06-09 11:38:52 +0100

    commit 1

--- animals.py
+++ animals.py
@@ -1,2 +1,44 @@
-class animal:
-    pass
+""" base class animal"""
+
+class animal: #blueprint (I want to describe what ALL animals are like)
+    
+    def __init__(self, name, sound):
+        self._name = name               #constructor (When I make an animal, I need a name and a sound)
+        self._sound = sound
+
+    def makesound(self):
+        print(f"{self._name} makes {self._sound}")
+
+class Dog(animal):
+    def __init__(self):
+        super().__init__("Dog", "bark")
+
+class Duck(animal):
+
+    def __init__(self):
+        super().__init__("Duck","Quack") 
+
+class Seal(animal):
+    def __init__(self):
+        super().__init__("Seal", "screech")
+
+lassy = Dog()
+
+#lassy.makesound()
+
+daffy = Duck()
+
+#daffy.makesound()
+
+salty = Seal()
+
+#salty.makesound()
+
+animals = []
+
+animals.append(lassy)
+animals.append(daffy)
+animals.append(salty)
+
+for animal in animals:
+    animal.makesound()
